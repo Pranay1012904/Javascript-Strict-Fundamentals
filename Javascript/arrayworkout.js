f8bef8c937ba4fd35a2da05fa9c5582289f93ddd
Pranay1012904 <pranay.dmnsns@gmail.com>
2026-03-06 21:24:57 +0530

    fundamentals

---
+++ Javascript/arrayworkout.js
@@ -0,0 +1,16 @@
+const calc = (bill) => {
+  let tip = 0;
+  if (bill >= 50 && bill <= 100) {
+    tip = (15 * bill) / 100;
+  } else {
+    tip = (20 * bill) / 100;
+  }
+  return tip;
+};
+
+const bills = new Array(125, 555, 44);
+const tips = [calc(bills[0]), calc(bills[1]), calc(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log("BILLS:", bills);
+console.log("TIPS", tips);
+console.log("TOTALS", totals);
---
+++ Javascript/object.js
@@ -0,0 +1,12 @@
+const obj = {
+  name: "Jonas",
+  friends: ["Michel", "Jon", "Lee"],
+};
+
+console.log(
+  obj.name +
+    " has" +
+    obj.friends.length +
+    " friends but his best friend is" +
+    obj.friends[0],
+);
